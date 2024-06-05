2191ce211d8cedfe52f79a4014613f5b9b882486
shimjaedoo <woen1637@naver.com>
2024-06-05 11:42:50 +0900

    [feat] Login component

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import {UseLoginQuery} from "../../queries/login.query";
+import {ILogin} from "../../interfaces/login.interface";
 
-function SignIn() {
+function Login() {
+    /** React-Query */
+    const { post }= UseLoginQuery()
+
+    /** Use-State */
+    const [login, setLogin] = useState<ILogin>({email : '', password : ''})
+    /** Use-Effect */
+
+
+    /** Functions */
+    function handleChange(e : React.ChangeEvent<HTMLInputElement>) {
+        const {name, value} = e.target
+        setLogin(e => ({...e, [name] : value}))
+    }
+
+    function create() {
+        post.mutate(login)
+    }
+
     return (
         <div className={'bg-gray-50 flex h-screen justify-center items-center'}>
         <div className={'w-4/12 h-85 bg-white flex flex-col gap-3 rounded-lg shadow-lg p-5'}>
         <p className={'text-2xl font-bold text-gray-700 text-center'}>로그인</p>
             <div className={'flex-col flex gap-3 py-5'}>
-    <input  type={'text'} className={'border-2 px-3 py-2 rounded-lg'} placeholder={'Email'} />
-    <input  type={'password'} className={'px-3 py-2 rounded-lg border'} placeholder={'Password'} />
+    <input  name='email' value={login.email} onChange={handleChange} type={'text'} className={'border-2 px-3 py-2 rounded-lg'} placeholder={'Email'} />
+    <input  name='password' value={login.password} onChange={handleChange} type={'password'} className={'px-3 py-2 rounded-lg border'} placeholder={'Password'} />
     </div>
-    <button className={'bg-blue-400 text-white text-sm py-1.5 rounded-lg hover:bg-blue-500 transition duration-300 active:bg-blue-600 shadow-xl'}>로그인</button>
+    <button
+        className={'bg-blue-400 text-white text-sm py-1.5 rounded-lg hover:bg-blue-500 transition duration-300 active:bg-blue-600 shadow-xl'}
+        onClick={create}
+    >로그인</button>
         </div>
         </div>
     )
 }
 
-export default SignIn
+export default Login
